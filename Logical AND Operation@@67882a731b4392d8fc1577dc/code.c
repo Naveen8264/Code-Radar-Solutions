768e8eccefb93d247174712bb30767d3fc77ed80
Naveen8264 <itzznaveen04@gmail.com>
2025-02-12 15:27:59 +0530

    Solution to Logical AND Operation

--- Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
+++ Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
@@ -5,7 +5,8 @@ int main() {
     scanf("%d%d", &a, &b);
     if (a >0&&b>0) {
         printf("True\n");  // Added semicolon here
-    } else {
+    } 
+    else {
         printf("False\n");
     }
     return 0;
